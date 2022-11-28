51d4f943d5c19bc4e2aeae0e3e40fdcc6a60e021
dnychennnn <yung-yu.chen@deliveryhero.com>
2022-11-28 16:02:05 +0100

    Class inheritance

--- src/main/java/basics/Person.kt
+++ src/main/java/basics/Person.kt
@@ -4,15 +4,38 @@ interface Person {
     val name: String
     val age: Int
     val canBuyAlcohol: Boolean
+        get() = age >= 21
 
     fun helloText(): String
 
     fun cheerText(person: Person): String
 }
 
+class Businessman(override val name: String, override val age: Int) : Person {
+
+    override fun helloText(): String {
+        return "Good morning"
+    }
+
+    override fun cheerText(person: Person): String {
+        return "Hello, my name is ${this.name}, nice to see you ${person.name}"
+    }
+
+}
+
+class Student(override val name: String, override val age: Int) : Person{
+    override fun helloText(): String {
+        return "Hi"
+    }
+
+    override fun cheerText(person: Person): String {
+        return "Hey ${person.name}, I am ${this.name}"
+    }
+}
+
 fun main(args: Array<String>) {
-    val businessman: Person = TODO("Use Businessman constructor here once it is implemented")
-    val student: Person = TODO("Use Student constructor here once it is implemented")
+    val businessman: Person = Businessman("Elon Musk", 46)
+    val student: Person = Student("Denny Chen", 20)
 
     println(businessman.helloText())
     println(student.helloText())
